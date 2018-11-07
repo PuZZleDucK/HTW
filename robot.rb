e6ee9a4c9b63bc3e49945fd01ac90e51ceb069b9
PuZZleDucK <puzzleduck@gmail.com>
2018-11-08 10:44:19 +1100

    Feature: basic place

--- robot.rb
+++ robot.rb
@@ -2,9 +2,16 @@
 puts "\nToy Robot Loaded"
 
 class Robot
+  attr_reader :x_location, :y_location, :facing
   def initialize
     @x_location = -1
     @y_location = -1
     @facing = ""
+  end
+
+  def place(x_new, y_new, facing_new)
+    @x_location = x_new
+    @y_location = y_new
+    @facing = facing_new
   end
 end
