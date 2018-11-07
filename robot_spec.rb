23f4c9e1c815ed056828749cac61f7dcf26439a8
PuZZleDucK <puzzleduck@gmail.com>
2018-11-08 10:46:41 +1100

    Test: test invalid place

--- robot_spec.rb
+++ robot_spec.rb
@@ -12,6 +12,30 @@ RSpec.describe Robot do
       expect(r.y_location).to eq(2)
       expect(r.facing).to eq("NORTH")
     end
+
+    it 'negative place command does not update' do
+      r = Robot.new()
+      r.place(-1,2,"NORTH")
+      expect(r.x_location).to eq(-1)
+      expect(r.y_location).to eq(-1)
+      expect(r.facing).to eq("")
+    end
+
+    it 'high place command does not update' do
+      r = Robot.new()
+      r.place(1,20,"NORTH")
+      expect(r.x_location).to eq(-1)
+      expect(r.y_location).to eq(-1)
+      expect(r.facing).to eq("")
+    end
+
+    it 'invalid facing command does not update' do
+      r = Robot.new()
+      r.place(1,2,"UP")
+      expect(r.x_location).to eq(-1)
+      expect(r.y_location).to eq(-1)
+      expect(r.facing).to eq("")
+    end
   end
 
   describe 'Tautology' do
